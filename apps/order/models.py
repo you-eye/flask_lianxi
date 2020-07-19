432970c90534db32243dd0bb3e5e6ac7ce3e38f6
zyc <1071325114@qq.com>
2020-07-19 20:56:52 +0800

    add order's models.py

--- apps/order/models.py
+++ apps/order/models.py
@@ -3,7 +3,7 @@ import datetime
 from ext import db
 
 
-class Goods(db.Model):
+class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(128), nullable=False)
     price = db.Column(db.Float, nullable=False)
